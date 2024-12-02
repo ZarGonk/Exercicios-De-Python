bb24a7d10f0703b992a860de0cb7ef0e05d5d754
Gustavo Rodrigues <gustavo.barreto013@hotmail.com>
2024-12-02 11:30:22 -0300

    Primeira Adição

--- 1. Entrada_e_Saida/(7-15)ExerciciosEasy.py
+++ 1. Entrada_e_Saida/(7-15)ExerciciosEasy.py
@@ -81,11 +81,9 @@ print("Seu salario agora é: {:.2f}".format(salario))
 
 '''
 #Desafio 15 - Faça a media aritmetica da nota de 1 aluno 
-'''
 n1 = float(input("Primeira nota: "))
 n2 = float(input("Segunda nota: "))
 
 media = (n1 + n2) / 2
 
 print("A media aritmetica entre {} e {} é igual a {:.2f}".format(n1, n2, media))
-'''
