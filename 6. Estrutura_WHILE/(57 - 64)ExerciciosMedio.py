a01758ca20b960f5de52f946ebc89401ee312f87
Gustavo Rodrigues <gustavo.barreto013@hotmail.com>
2024-12-10 14:51:45 -0300

    57 a 64 Exercícios

--- 6. Estrutura_WHILE/(57 - 64)ExerciciosMedio.py
+++ 6. Estrutura_WHILE/(57 - 64)ExerciciosMedio.py
@@ -1,7 +1,7 @@
 #Desafio 57 - faça um programa que leia o sexo de uma pessoa, mas só aceite os valores 'M' ou 'F'. Caso esteja errado, peça a digitação novamente até ter um valor correto.
 op = str(input('Qual seu sexo[M/F]? ')).strip().upper()[0]
 
-while op != 'M' and op != 'F':
+while op != 'M' and op != 'F': 
   print('\nOpção Invalida so [M] ou [F]')
   op = str(input('Qual seu sexo[M/F]? ')).strip().upper()[0]
 print('\nSexo {} Registrado com sucesso!!'.format(op))
