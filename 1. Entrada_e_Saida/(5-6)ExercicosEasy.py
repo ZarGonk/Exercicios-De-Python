a958012f1c14f43a68f9f5af279178167596a383
Gustavo Rodrigues <gustavo.barreto013@hotmail.com>
2025-01-09 12:10:34 -0300

    Separação de Exercicios

--- 1. Entrada_e_Saida/(5-6)ExercicosEasy.py
+++ 1. Entrada_e_Saida/(5-6)ExercicosEasy.py
@@ -8,12 +8,5 @@ print("é um numero", x.isalnum())
 print("é um espaço", x.isspace())
 '''
 
-#Desafio 6 - Faça uma Programa que leia dois numeros e mostre a soma
 
-v1 = int(input("Digite o primeiro valor: "))
-v2 = int(input("Digite o segundo valor: "))
-
-op = v1 + v2 
-
-print("Conta: {} + {} = {} ".format(v1, v2, op))
 
---
+++ 1. Entrada_e_Saida/6 - Exercicio.py
@@ -0,0 +1,8 @@
+#Desafio 6 - Faça uma Programa que leia dois numeros e mostre a soma
+
+v1 = int(input("Digite o primeiro valor: "))
+v2 = int(input("Digite o segundo valor: "))
+
+op = v1 + v2 
+
+print("Conta: {} + {} = {} ".format(v1, v2, op))
