e8dfe5bd0030128d8b268b785727c1814804b994
Gustavo Rodrigues <gustavo.barreto013@hotmail.com>
2025-01-09 12:12:31 -0300

    Separação de Exercícios

--- 1. Entrada_e_Saida/5 - Exercicio.py
+++ 1. Entrada_e_Saida/5 - Exercicio.py
@@ -1,12 +1,12 @@
 #Desafio 5 - Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele.
-'''
 x = input("Digite algo: ")
+
 print("o tipo primitivo é:", type(x))
 print("é numerico ?", x.isnumeric())
 print("é um alfabeto", x.isalpha())
 print("é um numero", x.isalnum())
 print("é um espaço", x.isspace())
-'''
+
 
 
 
