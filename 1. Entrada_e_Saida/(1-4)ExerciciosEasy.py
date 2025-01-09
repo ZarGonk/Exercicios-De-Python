25b21c5992e00732ca819426b908ce0886bdb0b3
Gustavo Rodrigues <gustavo.barreto013@hotmail.com>
2025-01-09 12:07:56 -0300

    Separação de Exercicios

--- 1. Entrada_e_Saida/(1-4)ExerciciosEasy.py
+++ 1. Entrada_e_Saida/(1-4)ExerciciosEasy.py
@@ -2,11 +2,4 @@
 
 '''print("olá,Mundo!")'''
 
-#desafio 4 - faça um codigo que leia 2 numeros e imprima a soma
 
-n1 = int(input("digite o primeiro número: "))
-n2 = int(input("digite o segundo numero: "))
-s = n1 + n2
-
-print("valor da soma é: ", s )
-print("valor da soma entre {} e {} é: {} ".format(n1, n2, s))
---
+++ 1. Entrada_e_Saida/4 - Exercicio.py
@@ -0,0 +1,8 @@
+#desafio 4 - faça um codigo que leia 2 numeros e imprima a soma
+
+n1 = int(input("digite o primeiro número: "))
+n2 = int(input("digite o segundo numero: "))
+s = n1 + n2
+
+print("valor da soma é: ", s )
+print("valor da soma entre {} e {} é: {} ".format(n1, n2, s))
