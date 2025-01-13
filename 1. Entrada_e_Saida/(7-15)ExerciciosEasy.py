c1cb226887e6e02a45219fe0bf86259322fc3f40
Gustavo Rodrigues <gustavo.barreto013@hotmail.com>
2025-01-13 15:23:31 -0300

    Separação de Exercicios

--- 1. Entrada_e_Saida/(7-15)ExerciciosEasy.py
+++ 1. Entrada_e_Saida/(7-15)ExerciciosEasy.py
@@ -6,24 +6,7 @@ suc = n + 1
 
 print("o antecessor: {} \no sucessor: {}".format(ant, suc))
 '''
-#Desafio 10 - Crie um programa que faça a taboada de 1 numero
-'''
-n = int(input("Qual taboada você deseja: "))
-um = n * 1
-dois = n * 2
-tres = n * 3
-quatro = n * 4
-cinco = n * 5
-seis = n * 6
-sete = n * 7
-oito = n * 8 
-nove = n * 9
-dez = n * 10
 
-print("Taboada")
-print("1 * {} = {} \n2 * {} = {}\n3 * {} = {}\n4 * {} = {}\n5 * {} = {}".format(n, um, n, dois, n, tres, n, quatro, n, cinco))
-print("6 * {} = {} \n7 * {} = {}\n8 * {} = {}\n9 * {} = {}\n10 * {} = {}".format(n,seis, n, sete, n, oito, n, nove, n, dez))
-'''
 #Desafio 11 - Crie um programa que leia quanto dinheiro uma pessoa tem, quantos dolares ela consegue comprar com esse dinheiro
 #considerando R$ 1.00 = R$ 3.27
 '''
---
+++ 1. Entrada_e_Saida/10 - Exercicio.py
@@ -0,0 +1,17 @@
+#Desafio 10 - Crie um programa que faça a taboada de 1 numero
+n = int(input("Qual taboada você deseja: "))
+um = n * 1
+dois = n * 2
+tres = n * 3
+quatro = n * 4
+cinco = n * 5
+seis = n * 6
+sete = n * 7
+oito = n * 8 
+nove = n * 9
+dez = n * 10
+
+print()
+print("Taboada":.^=)
+print("1 * {} = {} \n2 * {} = {}\n3 * {} = {}\n4 * {} = {}\n5 * {} = {}".format(n, um, n, dois, n, tres, n, quatro, n, cinco))
+print("6 * {} = {} \n7 * {} = {}\n8 * {} = {}\n9 * {} = {}\n10 * {} = {}".format(n,seis, n, sete, n, oito, n, nove, n, dez))
