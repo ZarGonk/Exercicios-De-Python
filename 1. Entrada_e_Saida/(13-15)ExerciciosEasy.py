0ec1f28b6a8629aef413aee29cd34d636df1bc2f
Gustavo Rodrigues <gustavo.barreto013@hotmail.com>
2025-01-13 19:42:11 -0300

    Separação de exercicios

--- 1. Entrada_e_Saida/(13-15)ExerciciosEasy.py
+++ 1. Entrada_e_Saida/(13-15)ExerciciosEasy.py
@@ -1,12 +1,3 @@
-#Desafio 7 - Faca um programa que leia um numero inteiro e mostre o antecessor e o sucessor desse numero
-'''
-n = int(input("Digite um valor: "))
-ant = n - 1
-suc = n + 1
-
-print("o antecessor: {} \no sucessor: {}".format(ant, suc))
-'''
-
 #Desafio 13 - Crie um programa que leio o preço do produto, e mostre seu novo preço de 5% de desconto
 '''
 v = float(input("qual é o valor desatualizado do produto? "))
---
+++ 1. Entrada_e_Saida/7 - Exercicio.py
@@ -0,0 +1,8 @@
+#Desafio 7 - Faca um programa que leia um numero inteiro e mostre o antecessor e o sucessor desse numero
+
+n = int(input("Digite um valor: "))
+ant = n - 1
+suc = n + 1
+
+print("o antecessor: {} \no sucessor: {}".format(ant, suc))
+
