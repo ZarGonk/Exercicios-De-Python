bc9664fa77068b29eea272e4c8cd9d80c19a6c29
Gustavo Rodrigues <gustavo.barreto013@hotmail.com>
2025-01-16 11:26:36 -0300

    Separação de Exercícios

--- 1. Entrada_e_Saida/(13-15)ExerciciosEasy.py
+++ 1. Entrada_e_Saida/(13-15)ExerciciosEasy.py
@@ -7,10 +7,4 @@ preco = v - desconto
 
 print("O valor atualizado do produto é: {:.2f}".format(preco))
 '''
-#Desafio 15 - Faça a media aritmetica da nota de 1 aluno 
-n1 = float(input("Primeira nota: "))
-n2 = float(input("Segunda nota: "))
 
-media = (n1 + n2) / 2
-
-print("A media aritmetica entre {} e {} é igual a {:.2f}".format(n1, n2, media))
---
+++ 1. Entrada_e_Saida/15 - Exercicio.py
@@ -0,0 +1,7 @@
+#Desafio 15 - Faça a media aritmetica da nota de 1 aluno 
+n1 = float(input("Primeira nota: "))
+n2 = float(input("Segunda nota: "))
+
+media = (n1 + n2) / 2
+
+print("A media aritmetica entre {} e {} é igual a {:.2f}".format(n1, n2, media))
