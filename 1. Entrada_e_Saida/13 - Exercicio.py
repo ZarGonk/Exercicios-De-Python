e14ad76da02d039ac6485dfc7c165ad79305d6fd
Gustavo Rodrigues <gustavo.barreto013@hotmail.com>
2025-01-16 11:30:13 -0300

    Entry and Exit exercises
    Python exercises from 1 to 15 - Canal Curso em Vídeo

--- 1. Entrada_e_Saida/13 - Exercicio.py
+++ 1. Entrada_e_Saida/13 - Exercicio.py
@@ -1,10 +1,8 @@
 #Desafio 13 - Crie um programa que leio o preço do produto, e mostre seu novo preço de 5% de desconto
-'''
+
 v = float(input("qual é o valor desatualizado do produto? "))
 
 desconto = v * 0.05
 preco = v - desconto
 
 print("O valor atualizado do produto é: {:.2f}".format(preco))
-'''
-
