a18bf80d6a192018b2544ebf690af3d6bcee6a46
Gustavo Rodrigues <gustavo.barreto013@hotmail.com>
2025-01-18 16:43:13 -0300

    Exercício 23

--- 3. Formatação_String/(22-27)ExerciciosEasy.py
+++ 3. Formatação_String/(22-27)ExerciciosEasy.py
@@ -9,19 +9,6 @@ primeiro_nome = len(nome.split()[0])  # Pega o primeiro nome
 
 print("Nome Maiusculo:{} \nNome Minusculos: {}\nSem Espaço: {}\nQuantidade de letras no primeiro nome: {}".format(M, m, len(uni), primeiro_nome))
 '''
-#Desafio 23 - faça um programa que leia um numero de 0 a 9999 e mostre na tela cada um dos digitos separados. ex: unidade: 4 dezena: 3 centena: 8 milhar: 1
-'''
-n = int(input("Digite um número entre 0 e 9999: "))
-
-# Extrai os dígitos individualmente
-u = (n // 1) % 10
-d = (n // 10) % 10
-c = (n // 100) % 10
-m = (n // 1000) % 10
-
-# Exibe os dígitos separadamente
-print("Milhar: {}\nCentena: {}\nDezena: {}\nUnidade: {}".format(m, 
-c, d, u))'''
 
 #Desafio 24 - crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome "SANTO".
 '''
---
+++ 3. Formatação_String/23 - Exercicio.py
@@ -0,0 +1,12 @@
+# Desafio 23 - faça um programa que leia um numero de 0 a 9999 e mostre na tela cada um dos digitos separados. ex: unidade: 4 dezena: 3 centena: 8 milhar: 1
+n = int(input("Digite um número entre 0 e 9999: "))
+
+# Extrai os dígitos individualmente
+u = (n // 1) % 10
+d = (n // 10) % 10
+c = (n // 100) % 10
+m = (n // 1000) % 10
+
+# Exibe os dígitos separadamente
+print("Milhar: {}\nCentena: {}\nDezena: {}\nUnidade: {}".format(m, 
+c, d, u))
