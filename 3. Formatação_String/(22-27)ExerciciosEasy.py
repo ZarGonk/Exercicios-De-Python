890bd82c246ea6d5061bffd168813da14a37cb27
Gustavo Rodrigues <gustavo.barreto013@hotmail.com>
2025-01-18 16:47:17 -0300

    Exercício 27

--- 3. Formatação_String/(22-27)ExerciciosEasy.py
+++ 3. Formatação_String/(22-27)ExerciciosEasy.py
@@ -13,14 +13,5 @@ print("Nome Maiusculo:{} \nNome Minusculos: {}\nSem Espaço: {}\nQuantidade de l
 
 
 
-#Desafio 27 - faça um programa que leia o nome completo de uma pessoa e mostre: o primeiro e o ultimo nome separadamente. Ex.: Ana Maria de Souza. Prinome: Ana Sobrenome: Souza
-
-name = str(input("Digite seu nome completo: ")).strip()
-
-c = name.split() #Separa o nome
-p = c[0] #primeiro nome
-u = c[-1] #ultimo nome
-
-print("Primeiro nome : {}\nUltimo nome: {}".format(p, u))
 
 
---
+++ 3. Formatação_String/27 - Exercicio.py
@@ -0,0 +1,9 @@
+#Desafio 27 - faça um programa que leia o nome completo de uma pessoa e mostre: o primeiro e o ultimo nome separadamente. Ex.: Ana Maria de Souza. Prinome: Ana Sobrenome: Souza
+
+name = str(input("Digite seu nome completo: ")).strip()
+
+c = name.split() #Separa o nome
+p = c[0] #primeiro nome
+u = c[-1] #ultimo nome
+
+print("Primeiro nome : {}\nUltimo nome: {}".format(p, u))
