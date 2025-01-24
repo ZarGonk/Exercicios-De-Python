d3ca9228a29eb1d5e1b4a2d5adc2a08d7748835a
Gustavo Rodrigues <gustavo.barreto013@hotmail.com>
2025-01-24 17:11:43 -0300

    Exercício 39

--- 4. Estrutura_De_Condição/(36-40)ExerciciosEasy.py
+++ 4. Estrutura_De_Condição/(36-40)ExerciciosEasy.py
@@ -1,31 +1,5 @@
 
-#Desafio 39 - Faça um programa que leia o ano de nascimento de um jovem e informe, de acordo com a sua idade, se ele ainda vai se alistar ao serviço militar, se é a hora exata de se alistar ou se já passou do tempo do alistamento. Seu programa também deverá mostrar o tempo que falta ou que passou do prazo.
-'''from datetime import date
 
-print('-----------------ALISTAMENTO-----------------')
-print('==='*15)
-
-ano = int(input("\nDigite Seu Ano De Nascimento: "))
-data = date.today().year #Data Atual
-idade = data - ano
-
-#Compara a idade se é menor que 18
-if idade < 18:
-  falta = 18 - idade
-  print('\nDaqui a algum tempo vai chegar a sua vez!!!\n\n\033[0;31mFalta {} Anos'.format(falta))
-#Compara a idade se é igual a 18
-elif idade == 18:
-  print('\033[0;32mVoce deve ir se alistar, \033[1;31mAGORA CORREE!!!')
-#Compara a idade se é maior que 18
-elif idade > 18:
-  print('\nJa se Alistou?\n\n1 - Sim\n2 - Não')
-  num = int(input('sim ou nao? '))
-  if num == 1:
-    print('\nTa salvo')
-  elif num == 2:
-    passou = idade - 18
-    print('\nTA DORMINDO FDP,ja passou o tempo\nTa a {} Anos Atrasado'.format(passou))
-'''
 #Desafio 40 - crie um programa que leia duas notas de um aluno e calcule sua média, mostrando uma mensagem no final, de acordo com a média atingida: - Média abaixo de 5.0: REPROVADO - Média entre 5.0 e 6.9: RECUPERAÇÂO - Média 7.0 ou superior: APROVADO.
 '''
 nt = float(input('Primeira Nota: '))
