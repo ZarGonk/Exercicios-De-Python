9504987668a62fccfbc5254d6c0efc461d1ab688
Gustavo Rodrigues <gustavo.barreto013@hotmail.com>
2025-01-24 17:14:07 -0300

    Exercício 30

--- 4. Estrutura_De_Condição/30 - Exercicios.py
+++ 4. Estrutura_De_Condição/30 - Exercicios.py
@@ -1,5 +1,4 @@
 # Desafio 30 - Escreva um programa que leia a velocidade de um carro. Se ele ultrapassa 80Km/h, mostre uma mensagem dizendo que ele foi multado. A multa vai custar R$7,00 por cada Km acima do limite.
-
 vel = float(input("Velocidade do Carro: "))
 
 if vel > 80:
