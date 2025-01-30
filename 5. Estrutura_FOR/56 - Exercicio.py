a5344e04c37d71f6b6b4ddf8f6975251ec8f99ae
Gustavo Rodrigues <gustavo.barreto013@hotmail.com>
2025-01-30 20:30:53 -0300

    Repeat for Structure

--- 5. Estrutura_FOR/56 - Exercicio.py
+++ 5. Estrutura_FOR/56 - Exercicio.py
@@ -1,6 +1,4 @@
-
-
-#Desafio 56 - Desenvolva um programa que leia o nome, idade e sexo de 4 pessoas. No final do programa, mostre: a média de idade do grupo, qual é o nome do homem mais velho e quantas mulheres têm menos de 20 anos.
+# Desafio 56 - Desenvolva um programa que leia o nome, idade e sexo de 4 pessoas. No final do programa, mostre: a média de idade do grupo, qual é o nome do homem mais velho e quantas mulheres têm menos de 20 anos.
 tot = maior = woman = 0
 velho = ''
 
