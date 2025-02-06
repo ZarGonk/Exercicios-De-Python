2da6e9dc49ac5ecdedc41f573e9de9e9b1e73bfa
Gustavo Rodrigues <gustavo.barreto013@hotmail.com>
2025-02-06 15:17:51 -0300

    Exercício 74

--- 7. Tuplas/(72 - 77)ExerciciosEasy.py
+++ 7. Tuplas/(72 - 77)ExerciciosEasy.py
@@ -13,26 +13,6 @@ else:
 
 
 
-#Exercício 74: Crie um programa que vai gerar cinco números aleatórios e 
-# colocar em uma tupla. Depois disso, mostre a listagem dos números gerados
-#e também indique o menor e o maior valor que estão na tupla.
-'''from random import randint
-
-num  = randint(1, 100)
-num1 = randint(1, 100)
-num2 = randint(1, 100) 
-num3 = randint(1, 100)
-num4 = randint(1, 100)
-
-aleatorio = (num, num1, num2, num3, num4)
-ordenado = sorted(aleatorio)
-
-print(f'Os números são:\n{aleatorio}')
-print(f'O Menor é {ordenado[0]}')
-print(f'O Maior é {ordenado[4]}')
-#outro jeito
-print(f'O menor é {min(aleatorio)}')
-print(f'O Maior é {max(aleatorio)}')'''
 
 #Exercício 75: Desenvolva um programa que leia quatro valores pelo teclado 
 #e guarde-os em uma tupla. No final, mostre:
