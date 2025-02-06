7d739889215f9c7b759fce1f43b488c7565ad61a
Gustavo Rodrigues <gustavo.barreto013@hotmail.com>
2025-02-06 15:21:36 -0300

    Exercício 77

--- 7. Tuplas/(72 - 77)ExerciciosEasy.py
+++ 7. Tuplas/(72 - 77)ExerciciosEasy.py
@@ -16,14 +16,3 @@ else:
 
 
 
-#Exercício 77: Crie um programa que tenha uma tupla com várias palavras 
-#(não usar acentos). Depois disso, você deve mostrar, para cada palavra, 
-#quais são suas vogais.
-palavras = ("banana", "livro", "oceano", "montanha", "tecnologia", 
-            "criatividade", "viagem", "planeta", "futuro", "aventura")
-
-for p in palavras:
-    print(f'\nNa palavra {p.upper()} temos: ',end='')
-    for letra in p:
-        if letra.lower() in 'aeiou':
-            print(letra, end=' ')
