f2e9ac2abaf0e8b287d77f9a2399ffd885516387
Gustavo Rodrigues <gustavo.barreto013@hotmail.com>
2025-02-11 16:18:06 -0300

    Exercício 80

--- 8. Listas/(78 - 83)ExerciciosMedio.py
+++ 8. Listas/(78 - 83)ExerciciosMedio.py
@@ -24,21 +24,7 @@ print(f'O maior valor add foi: {maior} na posição {posicoes_maior}°')
 print(f'O menor valor add foi: {menor} na posição {posicoes_menor}°')'''
 
 
-#Exercício 80: Crie um programa onde o usuário possa digitar cinco valores
-#numéricos e cadastre-os em uma lista, já na posição correta de inserção
-#(sem usar o sort()). No final, mostre a lista ordenada na tela.
-'''lista = []
-for i in range(5):
-    num = int(input(f'Digite {i} valor: '))
-    
-    if i == 0 or num > lista[-1]:
-        lista.append(num)
-    else:
-        posicao = 0
-        while posicao < len(lista) and num > lista[posicao]:
-            posicao += 1
-        lista.insert(posicao, num)
-print(lista)'''    
+
 
 #Exercício 81: Crie um programa que vai ler vários números e colocar em uma
 #lista. Depois disso, mostre:
