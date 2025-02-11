9b5c02489a3b739d451ad5b6c873eabbe1016e89
Gustavo Rodrigues <gustavo.barreto013@hotmail.com>
2025-02-11 16:18:55 -0300

    Exercício 81

--- 8. Listas/(78 - 83)ExerciciosMedio.py
+++ 8. Listas/(78 - 83)ExerciciosMedio.py
@@ -26,33 +26,7 @@ print(f'O menor valor add foi: {menor} na posição {posicoes_menor}°')'''
 
 
 
-#Exercício 81: Crie um programa que vai ler vários números e colocar em uma
-#lista. Depois disso, mostre:
-#Quantos números foram digitados.
-#A lista de valores, ordenada de forma decrescente.
-#Se o valor 5 foi digitado e está ou não na lista.
-'''lista = []
-while True:
-    lista.append(int(input('Enter a number: ')))
 
-    while True:
-        op = str(input('Want to continue[S/N]: ')).strip()[0]
-        if op in 'SsNn':
-            break
-        else:
-            print('Invalid Option. Sim (s) ou No (N): ')
-    if op in 'Nn':
-        break
-total = len(lista)
-print(f'Valor digitados: {total}')
-
-lista.sort(reverse=True)
-print(f'Lista na ordem decrescente: {lista}')
-
-if 5 in lista:
-    print('Valor 5 esta na lista')
-else:
-    print('Valor 5 não esta na lista')'''
 
 #Exercício 82: Crie um programa que vai ler vários números e colocá-los em 
 #uma lista. Depois disso, crie duas listas extras que vão conter apenas os 
