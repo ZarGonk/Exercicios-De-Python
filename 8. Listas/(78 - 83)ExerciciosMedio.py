498d2605886f21384906e07c250abc58aea210a0
Gustavo Rodrigues <gustavo.barreto013@hotmail.com>
2025-02-11 16:19:54 -0300

    Exercício 82

--- 8. Listas/(78 - 83)ExerciciosMedio.py
+++ 8. Listas/(78 - 83)ExerciciosMedio.py
@@ -28,31 +28,7 @@ print(f'O menor valor add foi: {menor} na posição {posicoes_menor}°')'''
 
 
 
-#Exercício 82: Crie um programa que vai ler vários números e colocá-los em 
-#uma lista. Depois disso, crie duas listas extras que vão conter apenas os 
-#valores pares e os valores ímpares digitados, respectivamente. 
-#Ao final, mostre o conteúdo das três listas geradas.
-'''lista = [], pares = [] , impares = []
-i = 0
-while True:
-    lista.append(int(input('Enter a Number: ')))
 
-    if lista[i] % 2 == 0:
-         pares.append(lista[i])
-    else:
-        impares.append(lista[i])
-    i += 1
-
-    stop = ' '
-    while stop not in 'SN':
-        stop = input('Want to Stop[S/N]: ').strip().upper()[0]
-    if stop == 'S':
-        break 
-     
-lista.sort()
-print(f'The initial list: {lista}')
-print(f'Pares Add: {pares}')
-print(f'Impares Add: {impares}')'''
 
 #Exercício 83: Crie um programa onde o usuário digite uma expressão qualquer 
 #que use parênteses. Seu aplicativo deverá analisar se a expressão passada 
