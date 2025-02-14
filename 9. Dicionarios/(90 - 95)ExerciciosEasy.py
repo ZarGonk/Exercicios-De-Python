72d22682b97cf439952e8f1f6c0688692d9a1eaf
Gustavo Rodrigues <gustavo.barreto013@hotmail.com>
2025-02-14 19:33:20 -0300

    Exercício 93

--- 9. Dicionarios/(90 - 95)ExerciciosEasy.py
+++ 9. Dicionarios/(90 - 95)ExerciciosEasy.py
@@ -27,32 +27,6 @@ for chave, valor in aluno.items():
 
 
 
-#Exercício 93: Crie um programa que gerencie o aproveitamento de um 
-# jogador de futebol. O programa vai ler o nome do jogador e quantas 
-# partidas ele jogou. Depois, vai ler a quantidade de gols feitos em 
-# cada partida e, no final, tudo isso será guardado em um dicionário, 
-# incluindo o total de gols feitos durante o campeonato.
-'''futebol = dict()
-gols = list()
-
-futebol['Nome'] = str(input('Nome do jogador: '))
-
-partidas = int(input('Quantas partidas Jogadas: '))
-for i in range(0, partidas):
-    gols.append(int(input(f'Quantos gols na partida {i+1}: ')))
-
-futebol['Gols'] = gols[:]
-futebol['Total'] = sum(gols)
-
-print('-=-'*15)
-print(futebol)
-print('-=-'*15)
-for c, v in futebol.items():
-    print(f'A chave {c} tem o valor {v}')
-print('-=-'*15)
-print(f'O jogador {futebol['Nome']} jogou {len(gols)} partidas')
-for i, g in enumerate(gols):
-    print(f'=> Na partida {i+1}, fez {g} gols')'''
 
 #Exercício 94: Crie um programa que leia nome, sexo e idade de várias 
 # pessoas, guardando os dados de cada pessoa em um dicionário e todos os
