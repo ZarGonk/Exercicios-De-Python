4d2ac5a75d56f4d2af39f2918fa50d6765ede067
Gustavo Rodrigues <gustavo.barreto013@hotmail.com>
2025-02-21 17:55:04 -0300

    Exercício 104

--- 10. Funções/(100 - 106)Exercicios_com_param_e_Return.py
+++ 10. Funções/(100 - 106)Exercicios_com_param_e_Return.py
@@ -4,29 +4,6 @@
 
 
 
-def ex104():
-#Exercício 104: Crie um programa que tenha a função chamada leiaInt(), que vai funcionar de forma semelhante à função input() do Python, só que fazendo a validação para aceitar apenas um valor numérico.
-#   Exemplo:
-#n = leiaInt('Digite um número: ')
-    def leiaInt(msg):
-        ok = False
-        valor = 0
-        
-        while True:
-            n = input(msg)
-            if n.isnumeric():
-                valor = int(n)
-                ok = True
-            else:
-                print('\033[0;31mERRO! Digite um numero válido.\033[m')
-            if ok == True:
-                break
-        return valor
-    
-
-    #Codigo principal
-    n = leiaInt('Digite um numero: ')
-    print(f'Voce acabou de digitar: {n}')
 
 def ex105():
 #Exercício 105: Faça um programa que tenha uma função chamada notas(), que pode receber várias notas de alunos e vai retornar um dicionário com as seguintes informações:
