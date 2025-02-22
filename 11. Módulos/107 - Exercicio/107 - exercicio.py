a79fdc803a3e7cfa26af29a047bda9614b9f2daf
Gustavo Rodrigues <gustavo.barreto013@hotmail.com>
2025-02-22 17:36:36 -0300

    Exercício 107

--- 11. Módulos/107 - Exercicio/107 - exercicio.py
+++ 11. Módulos/107 - Exercicio/107 - exercicio.py
@@ -1,4 +1,4 @@
-#Exercício 107: Crie um módulo chamado moeda.py que tenha as funções incorporadas:
+# Exercício 107: Crie um módulo chamado moeda.py que tenha as funções incorporadas:
 
 # aumentar(preço, taxa)
 # diminuir(preço, taxa)
@@ -12,4 +12,4 @@ preço = float(input('Digite o valor: R$ '))
 print(f'A metade de {preço} é {modulos.metade(preço)}')
 print(f'A dobro de {preço} é {modulos.dobro(preço)}')
 print(f'A aumento de {preço} é {modulos.aumenta(preço, taxa=10)}')
-print(f'A diminuir de {preço} é {modulos.diminuir(preço,taxa=13)}')
+print(f'A diminuir de {preço} é {modulos.diminuir(preço,taxa=13)}') 
