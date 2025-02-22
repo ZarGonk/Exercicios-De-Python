cdbb843b32a0c20f74c94e5b70daf12834596743
Gustavo Rodrigues <gustavo.barreto013@hotmail.com>
2025-02-22 17:42:38 -0300

    Exercício 110

--- 11. Módulos/110 - Exercicio/110 - exercicio.py
+++ 11. Módulos/110 - Exercicio/110 - exercicio.py
@@ -1,4 +1,4 @@
-#Exercício 110: Adicione ao módulo moeda.py uma função chamada resumo(), que mostre na tela algumas informações geradas pelas funções que você já criou até aqui, como o aumento e a redução de um valor, formatados em forma de tabela.
+# Exercício 110: Adicione ao módulo moeda.py uma função chamada resumo(), que mostre na tela algumas informações geradas pelas funções que você já criou até aqui, como o aumento e a redução de um valor, formatados em forma de tabela.
 import modulos
 
 preço = float(input('Digite o valor: R$ '))
