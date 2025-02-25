545a351cbaba94608a362319f478e81914b6bac8
Gustavo Rodrigues <gustavo.barreto013@hotmail.com>
2025-02-25 14:48:34 -0300

    Exercício 111

--- 12. Pacotes/111 - Exercicio/111 - exercicio.py
+++ 12. Pacotes/111 - Exercicio/111 - exercicio.py
@@ -1,4 +1,4 @@
-#Exercício 111: Crie um pacote chamado utilidadesCeV que tenha dois módulos internos chamados moeda e dado.
+# Exercício 111: Crie um pacote chamado utilidadesCeV que tenha dois módulos internos chamados moeda e dado.
 # Transfira todas as funções utilizadas nos exercícios 107, 108 e 109 para o primeiro pacote e mantenha tudo funcionado.
 
 from utilidadesCeV import moeda
