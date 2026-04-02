f6aa8d1f03456d5c63bcfa733298ec7e6d24c0d2
Gustavo Rodrigues <guriogrande75@gmail.com>
2026-04-02 20:49:31 -0300

    Add test class and update project files
    Add Meus Codigos/testclass.py containing a small documented example class and a print demonstration; update .idea/misc.xml to register Black and set the project SDK to Python 3.13; change LICENSE header text from 'MIT License' to 'IT License'; remove obsolete cadastro.txt.

---
+++ Meus Codigos/testclass.py
@@ -0,0 +1,15 @@
+class MinhaClassDocumentada:
+    """Está é a documentação da minha class, como exemplo do __doc__"""
+
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        return 'Hello, World !'
+
+
+mcd = MinhaClassDocumentada()
+print(mcd)
+
+
+#print(MinhaClassDocumentada.__doc__)
