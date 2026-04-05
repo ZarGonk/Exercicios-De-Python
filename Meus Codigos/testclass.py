43387d7dbac62bbdf42e00abe5a74cc153b297c8
Gustavo Rodrigues <gustavo.barreto013@hotmail.com>
2026-04-04 21:23:35 -0300

    Update testclass.py

--- Meus Codigos/testclass.py
+++ Meus Codigos/testclass.py
@@ -1,15 +1,18 @@
 class MinhaClassDocumentada:
     """Está é a documentação da minha class, como exemplo do __doc__"""
 
-    def __init__(self):
-        pass
+    def __init__(self, name=''):
+        self.name = name
 
     def __str__(self):
         return 'Hello, World !'
 
 
-mcd = MinhaClassDocumentada()
+mcd = MinhaClassDocumentada("Test")
 print(mcd)
 
 
-#print(MinhaClassDocumentada.__doc__)
+print(MinhaClassDocumentada.__doc__)
+print(mcd.__dict__)
+print(mcd.__getstate__())
+print(mcd.__class__)
