fcf170486922b233c3c5ba53dac746fe3aad8763
Gustavo Rodrigues <gustavo.barreto013@hotmail.com>
2026-04-07 20:01:27 -0300

    Exercício 119

--- 14. OOP/Desafios/119 - Exercicio.py
+++ 14. OOP/Desafios/119 - Exercicio.py
@@ -7,30 +7,35 @@ class Livro:
         self.title = title
         self.pages = pages
         self.current_page = 1
+        self.book = False
 
-    def open_book(self):
-        print(f":book:[blue] Você ababou de abrir o livro [red]'{self.title}'[/] que tem [green]{self.pages}[/] no total. Você agora está na [yellow]{self.current_page}[/]")
+    def open_book(self) -> bool:
+        self.book = True
 
-    
-    def avancar_paginas(self, value):
-        if self.current_page == self.pages:
-            print(f":rotating_light:[red]Você terminou o livro!'{self.title}'[/]")
+        print(f":book:[blue] Você acabou de abrir o livro [red]'{self.title}'[/] que tem [green]{self.pages}[/] no total. Você agora está na [yellow]{self.current_page}[/]")
 
-        count = 0
-        for _ in range(value):
-            if self.current_page < self.pages:
-                sleep(.4)
-                self.current_page += 1
-                count += 1
-                print(f'Pág{self.current_page} :right_arrow:  ', end='', flush=True)
-            else:
-                break
+    def avancar_paginas(self, value=1):
+        if self.book:
+            if self.current_page == self.pages:
+                print(f":rotating_light:[red]Você terminou o livro!'{self.title}'[/]")
 
-        print(f"[blue]Você avançou {count} páginas e agora está na [yellow]página {self.current_page}[/]")
+            count = 0
+            for _ in range(value):
+                if self.current_page < self.pages:
+                    sleep(.3)
+                    self.current_page += 1
+                    count += 1
+                    print(f'Pág{self.current_page} :right_arrow:  ', end='', flush=True)
+                else:
+                    break
 
-        if self.current_page == self.pages:
-            print(f":rotating_light:[red]Você chegou ao final do livro '{self.title}'[/]")
+            print(f"[blue]Você avançou {count} páginas e agora está na [yellow]página {self.current_page}[/]")
+
+            if self.current_page == self.pages:
+                print(f":rotating_light:[red]Você chegou ao final do livro '{self.title}'[/]")
+        else: print(f"📘 É necessario abrir o livro para ler")
 
 l1 = Livro('Magico de OS', 20)
-l1.avancar_paginas(15)
+l1.open_book()
+l1.avancar_paginas(5)
 l1.avancar_paginas(10)
