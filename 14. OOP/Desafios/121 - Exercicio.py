452cb1852a9d4da8a4a62994bfebb734d511042e
Gustavo Rodrigues <gustavo.barreto013@hotmail.com>
2026-04-07 15:19:19 -0300

    Add Caneta class example with colored output
    Add OOP exercise (Desafio 121): implements a Caneta class using rich to print colored messages. Provides methods destampar, escrever (handles azul/vermelho/verde) and quebra_linha, and demonstrates usage by instantiating three pens and printing sample messages.

---
+++ 14. OOP/Desafios/121 - Exercicio.py
@@ -0,0 +1,39 @@
+from rich import print
+
+class Caneta:
+    def __init__(self, cor):
+        self.cor = cor
+        self.destampada = False
+    
+    def destampar(self):
+        self.destampada = True
+
+    def quebra_linha(self):
+        print('\n')
+    
+    def escrever(self, mensagem):
+        if self.destampada:
+            if self.cor == "azul":
+                print(f"[blue]{mensagem}[/]")
+            
+            elif self.cor == "vermelho":
+                print(f"[red]{mensagem}[/]")
+            
+            elif self.cor == "verde":
+                print(f"[green]{mensagem}[/]")
+        else:
+            print('Caneta esta tampada!')
+
+
+c1 = Caneta('azul')
+c2 = Caneta('vermelho')
+c3 = Caneta('verde')
+
+c1.destampar()
+c2.destampar()
+c3.destampar()
+
+c1.escrever('Olá, Tudo bem? ')
+c1.quebra_linha()
+c2.escrever('Olá, consagrado! ')
+c3.escrever('Vem sempre aqui? ')
