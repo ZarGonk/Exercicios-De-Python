5f3a5686109c72e2242e5a57b94e52f660225185
Gustavo Rodrigues <gustavo.barreto013@hotmail.com>
2026-04-07 20:00:34 -0300

    Exercício 116

--- 14. OOP/Desafios/116 - Exercicio.py
+++ 14. OOP/Desafios/116 - Exercicio.py
@@ -1,13 +1,15 @@
 from rich import print
 
 class Funcionario:
+    empresa = "Curso em Video"
+    
     def __init__(self, name='Guests', sector='Mendigo', position='Desempregado'):
         self.name       = name
         self.sector     = sector
         self.position   = position
     
     def apresentação(self):
-        return f'Olá, sou [blue]{self.name}[/] e sou [blue]{self.position}[/] do setor de [green]{self.sector}[/] da empresa VAI QUE É TUA'
+        return f'Olá, sou [blue]{self.name}[/] e sou [blue]{self.position}[/] do setor de [green]{self.sector}[/] da empresa {Funcionario.empresa}'
 
 
 c1 = Funcionario("Maria", "Administração", "Diretora")
